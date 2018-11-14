e89cf908381e2ca516c6b36842ee776ba9425267
Aidil <riskisultan@yahoo.com>
2018-11-14 13:59:50 +0700

    Main program runnable.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import pygame
 from pygame.locals import *
 
 def DrawOrigin2D():
+    glColor3f(1, 1, 1)
     glBegin(GL_LINES)
     glVertex2fv([500, 0])
     glVertex2fv([-500, 0])
@@ -15,6 +16,7 @@ def DrawOrigin2D():
     glEnd()
 
 def DrawOrigin3D():
+    glColor3f(1, 1, 1)
     glBegin(GL_LINES)
     glVertex3fv([500, 0, 0])
     glVertex3fv([-500, 0, 0])
@@ -24,38 +26,52 @@ def DrawOrigin3D():
     glVertex3fv([0, 0, -500])
     glEnd()
 
+def Draw2D(points):
+    glColor3f(1, 0, 0)
+    glBegin(GL_POLYGON)
+
+    for point in points:
+        glVertex2fv(point)
+
+    glEnd()
+
 def main():
     program_mode = input('2D / 3D: ')
+    jumlah_titik = 0
 
     while program_mode != '2D' and program_mode != '3D':
         print('Input tidak valid!')
         program_mode = input('2D / 3D: ')
 
     if program_mode == '2D':
+
+        jumlah_titik = int(input('Jumlah titik: '))
+        titik = []
+        for i in range(0, jumlah_titik):
+            input_x, input_y = input().split()
+            input_x = float(input_x)
+            input_y = float(input_y)
+            titik.append([input_x, input_y])
+            
         pygame.init()
         display = (800, 600)
 
         pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-        console_input = ''
-
-        while not console_input == 'QUIT':
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
             DrawOrigin2D()
+            Draw2D(titik)
             pygame.display.flip()
-            console_input = input()
     elif program_mode == '3D':
         pygame.init()
         display = (800, 600)
         
         pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-        glTranslatef(0.0, 0.0, 0.0)
-        
         
         while True:
             for event in pygame.event.get():
